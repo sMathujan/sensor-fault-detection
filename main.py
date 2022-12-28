90311e470b4642683db15be4dfb61c52f3ffcd49
sMathujan <sivananthanmathujan@gmail.com>
2022-12-28 20:38:59 +0530

    logger code has been written and tested with main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from sensor.configuration.mongo_db_connection import MongoDBClient
 from sensor.exception import SensorException
 import os, sys
+from sensor.logger import logging
 
 def test_exception():
     try:
+        logging.info("We are dividing 1 by zero")
         x=1/0
     except Exception as e:
         raise SensorException(e, sys)
