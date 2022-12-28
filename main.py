e012d2570f755824362361446948737303d2dd7c
sMathujan <sivananthanmathujan@gmail.com>
2022-12-28 09:20:32 +0530

    exception code has been written and tested with main.py custom code

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from sensor.configuration.mongo_db_connection import MongoDBClient
+from sensor.exception import SensorException
+import os, sys
+
+def test_exception():
+    try:
+        x=1/0
+    except Exception as e:
+        raise SensorException(e, sys)
 
 if __name__ == "__main__":
-    mongodb_client = MongoDBClient()
-    print("collection name:", mongodb_client.database.list_collection_names())
+    try:
+        test_exception()
+    except Exception as e:
+        print(e)
+
+    # mongodb_client = MongoDBClient()
+    # print("collection name:", mongodb_client.database.list_collection_names())
