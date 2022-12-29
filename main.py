987d23e9651e70356584ad01d8f2b5c6d1bacb1f
sMathujan <sivananthanmathujan@gmail.com>
2022-12-29 12:27:55 +0530

    DataIngestionConfig has been defined and tested with main.py

--- main.py
+++ main.py
@@ -2,19 +2,10 @@ from sensor.configuration.mongo_db_connection import MongoDBClient
 from sensor.exception import SensorException
 import os, sys
 from sensor.logger import logging
+from sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig
 
-def test_exception():
-    try:
-        logging.info("We are dividing 1 by zero")
-        x=1/0
-    except Exception as e:
-        raise SensorException(e, sys)
 
 if __name__ == "__main__":
-    try:
-        test_exception()
-    except Exception as e:
-        print(e)
-
-    # mongodb_client = MongoDBClient()
-    # print("collection name:", mongodb_client.database.list_collection_names())
+    training_pipeline_config = TrainingPipelineConfig()
+    data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+    print(data_ingestion_config.__dict__)
