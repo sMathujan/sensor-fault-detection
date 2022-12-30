3faa1e7dee14b24176a66f5356bda616f3e560b1
sMathujan <sivananthanmathujan@gmail.com>
2022-12-30 09:46:22 +0530

    data_ingestion component and sensor_data.py have been coded

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -3,6 +3,7 @@ from sensor.entity.artifact_entity import DataIngestionArtifact
 from sensor.exception import SensorException
 import sys, os
 from sensor.logger import logging
+from sensor.components.data_ingestion import DataIngestion
 
 
 class TrainPipeline:
@@ -13,8 +14,12 @@ class TrainPipeline:
     
     def start_data_ingestion(self)->DataIngestionArtifact:
         try:
-            logging.info("Starting Data Ingestion")
-            logging.info("Data Ingestion Completed")
+            self.data_ingestion_config = DataIngestionConfig(training_pipeline_config=self.training_pipeline_config)
+            logging.info("Starting data ingestion")
+            data_ingestion = DataIngestion(data_ingestion_config=self.data_ingestion_config)
+            data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+            logging.info(f"Data ingestion completed and artifact: {data_ingestion_artifact}")
+            return data_ingestion_artifact
 
         except Exception as e:
             raise SensorException(e, sys)
@@ -22,7 +27,7 @@ class TrainPipeline:
 
     def run_pipeline(self):
         try:
-            self.start_data_ingestion()
+            data_ingestion_artifact:DataIngestionArtifact = self.start_data_ingestion()
 
         except Exception as e:
             raise SensorException(e, sys)
